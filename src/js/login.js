8f51e53f25efa82a6d68fb7137ba7ac0225cf184
Murilo <murilomendesmarques777@gmail.com>
2025-06-10 11:44:21 -0300

    Página de login e de tarefas funcionando

--- src/js/login.js
+++ src/js/login.js
@@ -1,18 +1,29 @@
 document.addEventListener('DOMContentLoaded',()=>{
-    
-    const loginForm = document.getElementById('form-login')
+
+    const loginForm =document.getElementById("form-login");
 
     if(loginForm){
         loginForm.addEventListener('submit',(e)=>{
             e.preventDefault();
 
+            const username=document.getElementById("username").value;
+            const paswword =document.getElementById("password").value;
 
-            const username=document.getElementById('username').ariaValueMax;
-            const password=document.getElementById('password').ariaValueMax;
+            localStorage.getItem("username");
+            localStorage.getItem("password");
 
-            if(username === 'admin' && password === "123456"){
+
+            if(username === "admin" && paswword === "123456"){
+
+                let token = Math.random().toString(16).substring(2)+
+                            Math.random().toString(16).substring(2)
+                            
+                localStorage.setItem("username",username);
+                localStorage.setItem("password",token);
+
+
                 setTimeout(()=>{
-                    window.location.href="index.html"
+                    window.location.href="index.html";
                 },1000)
             }
             else{
