3468820e7d5c08121ff3a77d2872c68123999358
Murilo <murilomendesmarques777@gmail.com>
2025-06-10 11:03:49 -0300

    Adicionando alertas para os botões

--- src/js/script.js
+++ src/js/script.js
@@ -15,15 +15,28 @@ function adicionarTarefa(){
     let linhas = document.getElementById("task")
 
     if(validarCampo()){
-        alert("Por favor, preencha o campo de tarefa.")
+        //alert("Por favor, preencha o campo de tarefa.")
+        Swal.fire({
+            icon:"warning",
+            title:"Atenção",
+            text:"Preencha o campo de tarefa",
+            confirmButtonColor:"#0E477CFF",
+            confirmButtontext:"OK"
+        })
     }
     else{
         tarefas.push(linhas.value)
         linhas.value="";
         listarTarefas();
-    }
+        Swal.fire({
+            icon:"success",
+            title:"Tarefa adiconada com sucesso",
+            showConfirmButton:false,
+            timer:1500,
+            
+    })
 }
-
+}
 //LISTAR TAREFAS
 
 function listarTarefas(){
